720949374e5b964ee8a39eacbf07fc451155ca62
yeedacoding <taeheun0325@naver.com>
2025-02-17 23:09:13 +0900

    data count

---
+++ data_copy/foreigner/check_data_count.py
@@ -0,0 +1,17 @@
+from data_copy.foreigner.data4yolo import checkFromDatasetCount, checkToDatasetCount
+
+org_data_root = '/Volumes/G-DRIVE USB'
+to_data_dir = 'dataset'
+
+version_num = ['3', '4', '5', '6', '7']
+
+# 원본 데이터의 각 버전별 개수 파악
+for version in version_num :
+    print("==============================")
+    print(f"원본 데이터 verison {version}, 'images'")
+    checkFromDatasetCount(org_data_root, version, "images")
+
+# 복사 완료된 경로의 데이터 총 갯수 파악
+print("==============================")
+print("복사 완료 데이터 총 합")
+checkToDatasetCount(org_data_root, to_data_dir, "images")

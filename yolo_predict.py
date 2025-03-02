2f3c009eba3e1f73d4d08cc03cefeced4cf8b4d6
yeedacoding <taeheun0325@naver.com>
2025-03-02 17:14:55 +0900

    test yolo prediction

--- yolo_predict.py
+++ yolo_predict.py
@@ -1,7 +1,9 @@
 from ultralytics import YOLO
 
 # Load a pretrained YOLO11n model
-model = YOLO("weights/yolov11n_20250214_e50b32/weights/best.pt")
+# model = YOLO("weights/yolov11n_20250214_e50b32/weights/best.pt")
+model = YOLO("weights/yolov11n_20250226_01_41_20_e50b32_dataset_calling_drinking_only/weights/best.pt")
+
 
 # Single stream with batch-size 1 inference
 # source = "udp://127.0.0.1:9090"  # RTSP, RTMP, TCP, or IP streaming address
@@ -11,6 +13,6 @@ model = YOLO("weights/yolov11n_20250214_e50b32/weights/best.pt")
 # results = model.predict(source="test.mov", show=True)
 
 ## 스트리밍
-results = model.predict(source=1, conf=0.1, show=True)
+results = model.predict(source=1, conf=0.25, show=True)
 
 print(results)
